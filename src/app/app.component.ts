341552faae889b4d325905efc80352e2d625a279
Sandeep309 <sandeepsaini6237@gmail.com>
2024-08-09 11:53:57 +0530

    REMOVE: primeNg + tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { PrimeNGConfig } from 'primeng/api';
-import { PrimengModule } from './modules/primeng/primeng.module';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PrimengModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  sidebarVisible: boolean = false;
-  constructor(private primeNgConfig: PrimeNGConfig) {}
-  ngOnInit() {
-    this.primeNgConfig.ripple = true;
-  }
+  constructor() {}
+
+  ngOnInit() {}
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { UsersComponent } from './components/users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'users', component: UsersComponent },
+];
